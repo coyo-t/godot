4b4dfa1003e72b330d92af45f99a9131ab21c099
The Birthday Coyote :] <constachugga@gmail.com>
2026-04-13 01:46:42 -0700

    i deleted the wrong thing

--- coyote/register_tha_coyote_typez.cpp
+++ coyote/register_tha_coyote_typez.cpp
@@ -1,6 +1,6 @@
 #include "register_tha_coyote_typez.h"
 
-// #include "fpw/block_render_manager.h"
+#include "fpw/block_render_manager.h"
 #include <core/object/class_db.h>
 #include <core/os/os.h>
 
@@ -9,7 +9,7 @@ auto register_tha_coyote_typez(int mode) -> void
 	OS::get_singleton()->yield();
 	switch (mode) {
 		case 0: { // register
-			// GDREGISTER_CLASS(BlockTextureManager)
+			GDREGISTER_CLASS(BlockTextureManager)
 			break;
 		}
 		case 1: { // unregister
